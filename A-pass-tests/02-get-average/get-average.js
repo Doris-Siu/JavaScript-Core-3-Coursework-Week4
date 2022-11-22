1479ececdea9046ccab0204c76daefd6de76a352
Doris-Siu <doris.siu.my@gmail.com>
2022-11-22 18:39:36 +0000

    Complete A-pass-tests

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,15 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+  let numArr = [];
+  for (let element of numbers) {
+    if (typeof element === "number") {
+      numArr.push(element);
+    }
+  }
+  let sum = numArr.reduce((p, c) => c + p);
+  return sum / numArr.length;
+}
 
 module.exports = average;
--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,22 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  let totals = {};
+  totals["Ford"] = carsSold
+    .filter((car) => car.make === "Ford")
+    .map((car) => car.price)
+    .reduce((p, c) => c + p);
+  totals["Honda"] = carsSold
+    .filter((car) => car.make === "Honda")
+    .map((car) => car.price)
+    .reduce((p, c) => c + p);
+  totals["Land Rover"] = carsSold
+    .filter((car) => car.make === "Land Rover")
+    .map((car) => car.price)
+    .reduce((p, c) => c + p);
+  totals["Toyota"] = carsSold
+    .filter((car) => car.make === "Toyota")
+    .map((car) => car.price)
+    .reduce((p, c) => c + p);
+  return totals;
+}
 
 module.exports = sales;
